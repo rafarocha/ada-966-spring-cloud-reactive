b927f6510aec9ad1e98a6c28b21a54fafa2575f9
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2023-03-08 20:57:45 -0300

    - remoção do hystrix deprecated - inclusao resilience4J - inclusao de spring cloud contract - experimento spring security

---
+++ pix-springsecurity/src/main/java/com/pix/PixSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixSpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixSpringsecurityApplication.class, args);
+    }
+
+}
---
+++ pix-springsecurity/src/test/java/com/pix/PixSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixSpringsecurityApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
